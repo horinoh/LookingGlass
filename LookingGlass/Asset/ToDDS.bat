802a2f046b45743dbe48b621348af7821539130c
horinoh <development@horino.sakura.ne.jp>
2024-05-23 22:50:38 +0900

    バッチ修正

--- LookingGlass/Asset/ToDDS.bat
+++ LookingGlass/Asset/ToDDS.bat
@@ -1,4 +1,5 @@
 REM texconv は DirectXTex にあるので、予めビルドして、環境変数 Path を通しておく必要がある
 REM texconv is in DirectXTex, build and set environment variable PATH, in advance
-@for %%i in (*.jpg) do @texconv %%i
-@for %%i in (*.png) do @texconv %%i
+REM -m 1 : miplevel 1
+@for %%i in (*.jpg) do @texconv -m 1 %%i
+@for %%i in (*.png) do @texconv -m 1 %%i
