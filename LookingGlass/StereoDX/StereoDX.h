70ec17ff34351c16e8e66a348a1518f5b01308b1
horinoh <development@horino.sakura.ne.jp>
2024-06-30 15:27:09 +0900

    コード更新

--- LookingGlass/StereoDX/StereoDX.h
+++ LookingGlass/StereoDX/StereoDX.h
@@ -30,14 +30,20 @@ public:
 	virtual void CreateTexture() override {
 		Super::CreateTexture();
 
+		constexpr auto HasColorMap = true;
+
 		constexpr auto Layers = 1;
-		AnimatedTextures.emplace_back().Create(COM_PTR_GET(Device), DisparityImage.cols, DisparityImage.rows, static_cast<UINT>(DisparityImage.elemSize()), Layers, DXGI_FORMAT_R8_UNORM, D3D12_RESOURCE_STATE_ALL_SHADER_RESOURCE);
+		AnimatedTextures.emplace_back().Create(COM_PTR_GET(Device), DisparityImage.cols, DisparityImage.rows, static_cast<UINT>(DisparityImage.elemSize()), Layers, DXGI_FORMAT_R8_UNORM, 
+			//!< カラーマップ専用テクスチャが無い場合は、ピクセルシェーダでも再利用する
+			HasColorMap ? D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE : D3D12_RESOURCE_STATE_ALL_SHADER_RESOURCE);
 		
-		//const auto& ColorMap = StereoImages[0];
-		//AnimatedTextures.emplace_back().Create(COM_PTR_GET(Device), ColorMap.cols, ColorMap.rows, static_cast<UINT>(ColorMap.elemSize()), Layers, DXGI_FORMAT_R8_UNORM, D3D12_RESOURCE_STATE_ALL_SHADER_RESOURCE);
+		if (HasColorMap) {
+			const auto& ColorMap = StereoImages[0];
+			AnimatedTextures.emplace_back().Create(COM_PTR_GET(Device), ColorMap.cols, ColorMap.rows, static_cast<UINT>(ColorMap.elemSize()), Layers, DrawGrayScale() ? DXGI_FORMAT_R8_UNORM : DXGI_FORMAT_R8G8B8A8_UNORM, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+		}
 	}
 
-	virtual const Texture& GetColorMap() const override { return AnimatedTextures[0]; };
+	virtual const Texture& GetColorMap() const override { return std::size(AnimatedTextures) > 1 ? AnimatedTextures[1]:AnimatedTextures[0]; };
 	virtual const Texture& GetDepthMap() const override { return AnimatedTextures[0]; };
 	virtual bool DrawGrayScale() const override { return true; }
 
@@ -48,8 +54,10 @@ public:
 				constexpr auto Layers = 1;
 				AnimatedTextures[0].UpdateUploadBuffer(DisparityImage.cols, DisparityImage.rows, static_cast<UINT>(DisparityImage.elemSize()), Layers, DisparityImage.ptr());
 
-				//const auto& ColorMap = StereoImages[0];
-				//AnimatedTextures[1].UpdateUploadBuffer(ColorMap.cols, ColorMap.rows, static_cast<UINT>(ColorMap.elemSize()), Layers, ColorMap.ptr());
+				if (std::size(AnimatedTextures) > 1) {
+					const auto& ColorMap = StereoImages[0];
+					AnimatedTextures[1].UpdateUploadBuffer(ColorMap.cols, ColorMap.rows, static_cast<UINT>(ColorMap.elemSize()), Layers, ColorMap.ptr());
+				}
 			}
 			Mutex.unlock();
 		}
@@ -59,7 +67,9 @@ public:
 
 		constexpr auto Bpp = 1;
 		AnimatedTextures[0].PopulateUploadToTextureCommand(DCL, Bpp);
-		//AnimatedTextures[1].PopulateUploadToTextureCommand(DCL, Bpp);
+		if (std::size(AnimatedTextures) > 1) {
+			AnimatedTextures[1].PopulateUploadToTextureCommand(DCL, Bpp);
+		}
 	}
 	virtual void UpdateWorldBuffer() override {
 		float X, Y;
--- LookingGlass/StereoVK/StereoVK.h
+++ LookingGlass/StereoVK/StereoVK.h
@@ -29,12 +29,16 @@ public:
 	virtual void CreateTexture() override {
 		Super::CreateTexture();
 		
+		constexpr auto HasColorMap = true;
+
 		AnimatedTextures.emplace_back().Create(Device, CurrentPhysicalDeviceMemoryProperties, VK_FORMAT_R8_UNORM, static_cast<uint32_t>(DisparityImage.elemSize()), VkExtent3D({ .width = static_cast<uint32_t>(DisparityImage.cols), .height = static_cast<uint32_t>(DisparityImage.rows), .depth = 1 }));
 
-		//const auto& ColorMap = StereoImages[0];
-		//AnimatedTextures.emplace_back().Create(Device, CurrentPhysicalDeviceMemoryProperties, VK_FORMAT_R8_UNORM, static_cast<uint32_t>(ColorMap.elemSize()), VkExtent3D({ .width = static_cast<uint32_t>(ColorMap.cols), .height = static_cast<uint32_t>(ColorMap.rows), .depth = 1 }));
+		if (HasColorMap) {
+			const auto& ColorMap = StereoImages[0];
+			AnimatedTextures.emplace_back().Create(Device, CurrentPhysicalDeviceMemoryProperties, DrawGrayScale() ? VK_FORMAT_R8_UNORM : VK_FORMAT_R8G8B8_UNORM, static_cast<uint32_t>(ColorMap.elemSize()), VkExtent3D({ .width = static_cast<uint32_t>(ColorMap.cols), .height = static_cast<uint32_t>(ColorMap.rows), .depth = 1 }));
+		}
 	}
-	virtual const Texture& GetColorMap() const override { return AnimatedTextures[0]; };
+	virtual const Texture& GetColorMap() const override { return std::size(AnimatedTextures) > 1 ? AnimatedTextures[1] : AnimatedTextures[0]; };
 	virtual const Texture& GetDepthMap() const override { return AnimatedTextures[0]; };
 	virtual bool DrawGrayScale() const override { return true; }
 
@@ -44,18 +48,25 @@ public:
 			{
 				AnimatedTextures[0].UpdateStagingBuffer(Device, DisparityImage.total() * DisparityImage.elemSize(), DisparityImage.ptr());
 
-				//const auto& ColorMap = StereoImages[0];
-				//AnimatedTextures[1].UpdateStagingBuffer(Device, ColorMap.total() * ColorMap.elemSize(), ColorMap.ptr());
+				if (std::size(AnimatedTextures) > 1) {
+					const auto& ColorMap = StereoImages[0];
+					AnimatedTextures[1].UpdateStagingBuffer(Device, ColorMap.total() * ColorMap.elemSize(), ColorMap.ptr());
+				}
 			}
 			Mutex.unlock();
 		}
 	}
 	virtual void PopulateAnimatedTextureCommand(const size_t i) override {
 		const auto CB = CommandBuffers[i];
-		AnimatedTextures[0].PopulateStagingToImageCommand(CB, DisparityImage.cols, DisparityImage.rows, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT | VK_PIPELINE_STAGE_TESSELLATION_EVALUATION_SHADER_BIT);
-
-		//const auto& ColorMap = StereoImages[0];
-		//AnimatedTextures[1].PopulateStagingToImageCommand(CB, ColorMap.cols, ColorMap.rows, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT);
+		AnimatedTextures[0].PopulateStagingToImageCommand(CB, DisparityImage.cols, DisparityImage.rows,
+			//!< カラーマップ専用テクスチャが無い場合は、フラグメントシェーダでも再利用する
+			(std::size(AnimatedTextures) > 1 ? 0 : VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT) |
+			VK_PIPELINE_STAGE_TESSELLATION_EVALUATION_SHADER_BIT);
+		
+		if (std::size(AnimatedTextures) > 1) {
+			const auto& ColorMap = StereoImages[0];
+			AnimatedTextures[1].PopulateStagingToImageCommand(CB, ColorMap.cols, ColorMap.rows, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT);
+		}
 	}
 	virtual void UpdateWorldBuffer() override {
 		float X, Y;
